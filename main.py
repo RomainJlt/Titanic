7ebe85e1e1c106175a2dc5b9830824298ddf453f
Paul Decatoire <decatopaul@gmail.com>
2025-03-11 15:58:28 +0100

    Rajout des conditions

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ import matplotlib as mpl
 df = pd.read_csv("data/gender_submission.csv")
 
 print(df.head())
+
+from ydata_profiling import ProfileReport
+
+profile = ProfileReport(df, title="Profile Report")
+profile.to_file("your_report.html")
